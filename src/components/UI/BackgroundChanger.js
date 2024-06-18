4ecbb2434dd2c51a14fcf0a03a3dfdfb03a96527
Joshua Smith <kazenostigma50@gmail.com>
2024-06-18 12:10:20 -0500

    fixed background changer

--- src/components/UI/BackgroundChanger.js
+++ src/components/UI/BackgroundChanger.js
@@ -1,32 +1,63 @@
 'use client';
 
-import {useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const images = [
   "/images/loginPageImages/anime.jpg",
   "/images/loginPageImages/anime2.jpg",
-  "/images/loginPageImages/rengoku.png",
+  "/images/loginPageImages/anime3.jpg",
   "/images/loginPageImages/fatestaynight.jpg"
 ];
 
 function BackgroundChanger({ children }) {
-  const [currentImage, setCurrentImage] = useState(0);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentImage((currentImage) => (currentImage + 1) % images.length);
-    }, 4000);
+    // Preload images
+    const preloadImages = async () => {
+      const promises = images.map((src) => {
+        return new Promise((resolve, reject) => {
+          const img = new Image();
+          img.src = src;
+          img.onload = resolve;
+          img.onerror = reject;
+        });
+      });
+      await Promise.all(promises);
+      setIsLoaded(true);
+    };
 
-    return () => clearInterval(intervalId);
+    preloadImages();
   }, []);
+
+  if (!isLoaded) {
+    return null; // or a loader/spinner component
+  }
 
   return (
     <div className="background-changer flex min-h-screen flex-1 flex-col justify-center px-6 lg:px-8">
       {children}
       <style jsx>{`
+        @keyframes backgroundChange {
+          0% {
+            background-image: url('/images/loginPageImages/anime.jpg');
+          }
+          25% {
+            background-image: url('/images/loginPageImages/anime2.jpg');
+          }
+          50% {
+            background-image: url('/images/loginPageImages/anime3.jpg');
+          }
+          75% {
+            background-image: url('/images/loginPageImages/fatestaynight.jpg');
+          }
+          100% {
+            background-image: url('/images/loginPageImages/anime.jpg');
+          }
+        }
+
         .background-changer {
-          background-image: url(${images[currentImage]});
-          transition: background-image 1s ease-in-out;
+          animation: backgroundChange 16s infinite ease-in-out;
           background-size: cover;
           background-position: center;
           background-repeat: no-repeat;
